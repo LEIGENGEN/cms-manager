19fe62cfcafa462d88e181b7110e1297c25d0454
雷敢当 <905627019@qq.com>
2024-12-08 19:29:52 +0800

    左侧列表样式

---
+++ src/pages/decorate/components/PageLeft.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-collapse v-model="activeNames">
+      <el-collapse-item v-for="(item, index) in componentlist" :key="index" class="component-item" :title="item.title"
+        :name="index">
+        <ul class="component-list">
+          <li v-for="(component, size) in item.components" :key="size">
+            <i :class="component.iconClass"></i>
+            <p>{{ component.name }}</p>
+            <p>4/50</p>
+          </li>
+        </ul>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
+
+<script>
+import componentlist from '../../../config/component-list';
+export default {
+  data() {
+    return {
+      activeNames: [1],
+      componentlist,
+    }
+  },
+}
+</script>
